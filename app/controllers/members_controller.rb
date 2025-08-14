f7dc6b3d8cfc6a2dd02acd5b544e583b8fed5f70
Gil DAYAN <gil.dayan@orange.fr>
2025-08-14 18:39:38 +0200

    Création de la méthode index de member controller, de la vue index et du lien dans la sidebar

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,10 @@
 class MembersController < ApplicationController
   skip_before_action :authenticate_user!
 
+  def index
+    @members = Member.all
+  end
+
   def show
     @member = Member.find_by!(slug: params[:slug])
   end
