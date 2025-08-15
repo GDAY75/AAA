cf00d60e6fdaf90bc820f4fcde5c36ff638c1392
Gil DAYAN <gil.dayan@orange.fr>
2025-08-15 15:20:57 +0200

    Création la vue de l'index pieces, de la méthode index et show du controller piece et du lien de la sidebar vers l'index des pieces

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,17 @@
 class PiecesController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_piece, only: :show
+
+  def index
+    @pieces = Piece.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_piece
+    @piece = Piece.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :members, param: :slug, only: [:show, :index]
-
+  resources :pieces, only: [:index, :show]
 end
