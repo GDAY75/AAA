690a1a017557ddf92a4a03bf5f5991f0fd16d065
Gil DAYAN <gil.dayan@orange.fr>
2025-08-23 12:41:41 +0200

    Styles des cards members et création des liens vers la show des members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,7 @@ class MembersController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @members = Member.all
+    @members = Member.order(:first_name)
   end
 
   def show
