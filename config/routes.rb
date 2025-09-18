bfb87df7f7a6c314aaa22336151d351e938f9a8a
Gil DAYAN <gil.dayan@orange.fr>
2025-09-18 20:07:33 +0200

    Création des routes pour une page contact

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :members, param: :slug, only: [:show, :index]
   resources :pieces, only: [:index, :show]
+  get  "contact", to: "contacts#new"
+  post "contact", to: "contacts#create"
 end
