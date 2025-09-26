4a356aef0b3770137f7746670209a22642cdad6a
Gil DAYAN <gil.dayan@orange.fr>
2025-09-26 20:21:58 +0200

    Affichage des messages d'erreurs et de validation du formulaire de contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,23 +2,24 @@ class ContactsController < ApplicationController
   skip_before_action :authenticate_user!
   def new
     @contact = OpenStruct.new(name: "", email: "", subject: "", message: "")
+    @errors = []
+    @success = "Merci ! Votre message a bien été envoyé." if params[:success].present?
   end
 
   def create
     @contact = OpenStruct.new(contact_params)
 
-    errors = []
-    errors << "Nom requis" unless @contact.name.present?
-    errors << "Email requis" unless @contact.email.present?
-    errors << "Message requis" unless @contact.message.present?
+    @errors = []
+    @errors << "Nom requis" unless @contact.name.present?
+    @errors << "Email requis" unless @contact.email.present?
+    @errors << "Message requis" unless @contact.message.present?
 
-    if errors.any?
-      flash.now[:alert] = errors.join(" • ")
+
+    if @errors.any?
       render :new, status: :unprocessable_entity
     else
       # Ici tu pourrais appeler un Mailer plus tard.
-      flash[:notice] = "Merci ! Votre message a bien été envoyé."
-      redirect_to contact_path
+      redirect_to contact_path(success: 1)
     end
   end
 
