1eb49c9a8fb466467de95d797b7f0c60b5bfb116
Gil DAYAN <gil.dayan@orange.fr>
2025-11-28 20:16:05 +0100

    Disable JS compressor in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # Désactiver la compression CSS pour éviter le bug SassC 'vw' vs 'px'
   config.assets.css_compressor = nil
+  config.assets.js_compressor = nil
 
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
