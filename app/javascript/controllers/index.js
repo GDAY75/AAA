423040bdb51986e919e9431302fb568d5598fd79
Gil DAYAN <gil.dayan@orange.fr>
2025-11-28 21:02:09 +0100

    Correction de index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,15 +1,18 @@
 import { Application } from "@hotwired/stimulus"
-import SidebarController from "./sidebar_controller"
-import LightboxController from "./lightbox_controller"
-import MasonryController from "./masonry_controller"
-import VideothumbController from "./videothumb_controller"
-import VideomodalController from "./videomodal_controller"
-import GalcoverController from "./galcover_controller"
+
+import SidebarController from "./sidebar_controller.js"
+import LightboxController from "./lightbox_controller.js"
+import MasonryController from "./masonry_controller.js"
+import VideothumbController from "./videothumb_controller.js"
+import VideomodalController from "./videomodal_controller.js"
+import GalcoverController from "./galcover_controller.js"
 
 const application = Application.start()
+
 application.register("sidebar", SidebarController)
 application.register("lightbox", LightboxController)
 application.register("masonry", MasonryController)
 application.register("videothumb", VideothumbController)
 application.register("videomodal", VideomodalController)
 application.register("galcover", GalcoverController)
+
