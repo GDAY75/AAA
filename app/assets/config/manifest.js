55d04d9b4924de0de08dfdc280055d4555cb4a1e
Gil DAYAN <gil.dayan@orange.fr>
2025-11-28 20:41:20 +0100

    Clean manifest.js for Rails 7 + importmap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,4 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link_tree ../videos
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link popper.js
-//= link bootstrap.min.js
+//= link application.css
+
